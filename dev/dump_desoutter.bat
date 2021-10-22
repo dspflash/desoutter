a1f60712e2228b066b1889a4556885ad06933aa9
dspflash <sardine@3ucs.com>
2021-10-22 16:28:46 +0800

    initial

--- dev/dump_desoutter.bat
+++
@@ -1,4 +0,0 @@
-echo on
-mysqldump -u root -p --databases desoutter >desoutter.sql --triggers --routines
-::mysqldump -u root -p --databases vtigercrm6 >vtigerpostgresql.sql --compatible=postgresql --default-character-set=utf8 --triggers --routines
-pause
